199ada76acde10eb54d52a8af55681d2c4204c04
Wyatt Pearsall <wyattttttt@gmail.com>
2014-08-24 23:27:23 -0700

    index updating function added to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+
 module.exports = function(grunt){
 	grunt.initConfig({
 		uglify:{
@@ -20,7 +23,23 @@ module.exports = function(grunt){
 	
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-watch");
-
+  grunt.loadNpmTasks("grunt-gh-pages"); 
 
 	grunt.registerTask('default',['uglify'])
+  
+  grunt.registerTask('makeIndex','Encode minified script and update index.html',
+    function(grunt){
+      var done = this.async();
+      var $ = cheerio.load(fs.readFileSync('index.html'));
+      var encoded = encodeURIComponent(fs.readFileSync('./spree.min.js'));
+      $('a').eq(0).attr('href',"javascript:" + encoded);
+
+      fs.writeFile('index.html',$.html(),function(err){
+        if(err) return grunt.log.error("\nCouldn't write output html.\n");
+        done(); 
+      });
+
+    });
+
+  grunt.registerTask('pages',['uglify','makeIndex','gh-pages']);
 };
